93a5e5b3a4f4a5706faf726328d7cbddc8720a0d
wonder <waxz@users.noreply.github.com>
2024-07-28 23:12:49 +0800

    add choose language button. remove txt file

--- app.py
+++ app.py
@@ -1,30 +1,57 @@
+import matplotlib
+matplotlib.use('Agg')
 import gradio as gr
 import edge_tts
 import asyncio
 import os
 import json
+
+from urllib.request import urlopen
+
+voices_list_body_url = "https://speech.platform.bing.com/consumer/speech/synthesize/readaloud/voices/list?trustedclienttoken=6A5AA1D4EAFF4E9FB37E23D68491D6F4"
+with urlopen(voices_list_body_url) as response:
+    voices_list_body = response.read()
+
+voices_list = json.loads(voices_list_body)
+
+
 # https://speech.platform.bing.com/consumer/speech/synthesize/readaloud/voices/list?trustedclienttoken=6A5AA1D4EAFF4E9FB37E23D68491D6F4
-with open("voice.txt", "r", encoding="utf-8") as f:
-    voice_data = json.load(f)
+# with open("voice.txt", "r", encoding="utf-8") as f:
+#     voice_data = json.load(f)
+    
+SUPPORTED_LANGUAGE = 'en'
+voice_data = [ voice for voice in voices_list if voice['Locale'].startswith(SUPPORTED_LANGUAGE)]
 
 # Create a dictionary of supported voices
 SUPPORTED_VOICES = {
     f"{voice['Gender']}-{voice['ShortName']}": voice['ShortName']
     for voice in voice_data
 }
+CHOSEN_VOICE = ""
+print("SUPPORTED_VOICES ", SUPPORTED_VOICES )
+
+ 
+    
 
 # 发音切换
 def changeVoice(voices):
+    global SUPPORTED_LANGUAGE, voice_data, SUPPORTED_VOICES, CHOSEN_VOICE
+    print("changeVoice SUPPORTED_VOICES ", SUPPORTED_VOICES )
     example = SUPPORTED_VOICES[voices]
+    CHOSEN_VOICE = example
+    print("changeVoice CHOSEN_VOICE ", CHOSEN_VOICE )
     # example_file = os.path.join(os.path.dirname(__file__), "example/"+example+".wav")
     # return example_file
     # 以上两行给注释掉了，因为我没制作相关的音频。
     return
 
 # 文本转语音
-async def textToSpeech(text, voices, rate, volume):
+async def textToSpeech(text, rate, volume):
     output_file = "output.mp3"
-    voices = SUPPORTED_VOICES[voices]
+    
+    voices = CHOSEN_VOICE # SUPPORTED_VOICES[voices]
+    print("textToSpeech CHOSEN_VOICE ", CHOSEN_VOICE )
+    print("textToSpeech voices ", voices )
     if (rate >= 0):
         rates = rate = "+" + str(rate) + "%"
     else:
@@ -66,17 +93,30 @@ with gr.Blocks(css="style.css", title="文本转语音") as demo:
             text = gr.TextArea(label="文本", elem_classes="text-area", lines=15)
             btn = gr.Button("生成", elem_id="submit-btn")
         with gr.Column():
-            voices = gr.Dropdown( choices=list(SUPPORTED_VOICES.keys()),  # Use the keys from SUPPORTED_VOICES
-    value="Male-zh-CN-YunxiNeural",  # Set a default voice
+            language =  gr.Dropdown( choices = ["en","zh","fr","de"], value="en",label="language",info="choose language", interactive=True ) 
+            
+            @gr.render(inputs=language)
+            def show_voice_drop_downs(lang):
+                global SUPPORTED_LANGUAGE, voice_data, SUPPORTED_VOICES, CHOSEN_VOICE
+                SUPPORTED_LANGUAGE = lang
+                print("show_voice_drop_downs, SUPPORTED_LANGUAGE: ", SUPPORTED_LANGUAGE)
+                voice_data = [ voice for voice in voices_list if voice['Locale'].startswith(SUPPORTED_LANGUAGE)]
+
+                # Create a dictionary of supported voices
+                SUPPORTED_VOICES = {f"{voice['Gender']}-{voice['ShortName']}": voice['ShortName']  for voice in voice_data }
+            
+                voices = gr.Dropdown( choices=list(SUPPORTED_VOICES.keys()),  # Use the keys from SUPPORTED_VOICES
+    value="",  # Set a default voice
     label="发音",
     info="请选择发音人",                                 interactive=True)
+                voices.change(fn=changeVoice,inputs=voices,outputs=example)
 
             example = gr.Audio(label="试听",
                               value="example/zh-CN-YunxiNeural.wav",
                               interactive=False,
                               elem_classes="example")
 
-            voices.change(fn=changeVoice,inputs=voices,outputs=example)
+            
             rate = gr.Slider(-100,
                              100,
                              step=1,
@@ -97,10 +137,10 @@ with gr.Blocks(css="style.css", title="文本转语音") as demo:
                              elem_classes="audio")
             clear = gr.Button("清除", elem_id="clear-btn")
             btn.click(fn=textToSpeech,
-                      inputs=[text, voices, rate, volume],
+                      inputs=[text, rate, volume],
                       outputs=[audio])
             clear.click(fn=clearSpeech, outputs=[text, audio])
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7880)
+    demo.launch(server_name="0.0.0.0", server_port=7880, share=False)
 
